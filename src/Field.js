4ca4067a5b10c69b7f4c0af6140066d739fa1ead
Celoslav <lucicmladen83@gmail.com>
2018-01-03 14:07:57 +0100

    User reports fixed

--- src/Field.js
+++ src/Field.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './field.css';
 
 class Field extends Component {
-
+    
+    state={
+        showReport:false,
+        addReport:false,
+    }
     render() {
         return (
             <div className="fieldBack">
                 <h3 className="fieldTitle">Add new report</h3>
                 <div className="fieldButton">
-                    <strong className="fieldPlus">+</strong>
+                    <strong className="fieldPlus" onClick={()=>{this.state.addReport()}}>+</strong>
                 </div>
             </div>
         );
