1a8f9e83e3f5dc4cb5f3a6e0440c2a263f777820
Stefan <stefansukara55@gmail.com>
2018-01-03 16:00:41 +0100

    Created AddReport component and addReport.css

---
+++ src/AddReport.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './addReport.css';
+
+class AddReport extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default AddReport;
