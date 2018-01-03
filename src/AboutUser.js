14feb6a537f37e898c6c08db9079935d5b9d31ca
Stefan <stefansukara55@gmail.com>
2018-01-03 22:13:55 +0100

    Installed react-google-maps with npm

--- src/AboutUser.js
+++ src/AboutUser.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import UserDetail from './UserDetail';
 import Field from './Field';
+// import AddReport from './AddReport';
 import './aboutUser.css';
 
 class AboutUser extends Component {
 
+    // state = {
+    //     addReport: false,
+    // }
+
+    // handleAddReport = () => {
+    //     this.setState({
+    //         addReport: true,
+    //     })
+    // }
+
     render() {
+        // if (!AddReport) {
         return (
             <div className="aboutUser">
                 <UserDetail
@@ -15,10 +27,25 @@ class AboutUser extends Component {
                     position={this.props.position}
                     reports={this.props.reports}
                     description={this.props.description}
+                // addReport={this.handleAddReport}
                 />
                 <Field />
             </div>
         );
+        // } else return(
+        //     <div className="aboutUser">
+        //             <UserDetail
+        //                 key={this.props.id}
+        //                 firstName={this.props.firstName}
+        //                 lastName={this.props.lastName}
+        //                 position={this.props.position}
+        //                 reports={this.props.reports}
+        //                 description={this.props.description}
+        //                 addReport={this.handleAddReport}
+        //             />
+        //             <AddReport />
+        //         </div>
+        // )
     }
 }
 
--- src/AddReport.js
+++ src/AddReport.js
@@ -1,14 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './addReport.css';
 
-class AddReport extends Component{
-    render(){
-        return(
-            <div>
-                
+class AddReport extends Component {
+    render() {
+        return (
+            <div className="field">
+                <form method="post" className="form-newReport"  >
+                    <div className="rowDate">
+                        <label>Datum polaska: </label>
+                        <input id="date1" />
+                        <label>Datum dolaska: </label>
+                        <input id="date2" />
+                    </div>
+                    <label>Dnevnica : </label>
+                    <label>domaća</label>
+                    <input type="radio" name="radioButton" value="domaća" id="choice2" defaultChecked />
+                    <label>EX-YU</label>
+                    <input type="radio" name="radioButton" value="EX-YU" id="choice1" />
+                    <label>strana</label>
+                    <input type="radio" value="strana" name="radioButton" id="choice3" />
+                    <br />
+                    <label>Troškove snosi : </label>
+                    <div type="buttom" className="buttom-costs">
+                        <select id="dropdownItem">
+                            <option name="dropdowna" value="kompanija">Kompanija</option>
+                            <option name="dropdowna" value="zaposleni">Zaposleni</option>
+                        </select>
+                    </div>
+                    <label>Lokacija : </label>
+                    <input type="text" id="mapsearch" required="" />
+                    <div id="mapElement" >
+                    </div>
+                </form>
             </div>
-        )
+        );
     }
 }
-
 export default AddReport;
