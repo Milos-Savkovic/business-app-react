bc4d29fa00d4e934e674bf3f539af84808a5f05e
Celoslav <lucicmladen83@gmail.com>
2018-01-16 17:00:08 +0100

    ...table

--- src/components/ReportTable.js
+++ src/components/ReportTable.js
@@ -3,13 +3,13 @@ import './ReportTable.css';
 
 export const ReportTable = (props) => {
   const style = {
-    borderTop: {borderTop: '1px solid #333'},
+    borderTop: { borderTop: '1px solid #333' },
   }
   return (
     <div className="table-container">
       <div className="table-row table-row-1">
         <div className="cell cell-1"><p className="cell-1__text">Dnevnice</p></div>
-        <div className="cell cell-2">
+        <div className="cell table-row-cell-2">
           <div className="cell-2-row">
             Dan odlaska
             <span className="underline"></span>
@@ -50,21 +50,66 @@ export const ReportTable = (props) => {
         <div className="cell cell-1 table-row-2-cell-1">
           Prevozni troškovi
         </div>
-        <div className="cell cell-2 table-row-2-cell-1 long-field">
-          <div className="cell-2-row"></div>
-          <div className="cell-2-row"></div>
-          <div className="cell-2-row"></div>
-          <div className="cell-2-row"></div>
-          <div className="cell-2-row"></div>
-          <div className="cell-2-row"></div>
-          <div className="cell-2-row"></div>
-          <div className="cell-2-row"></div>
+        <div className="cell cell-2 table-row-2-cell-2">
+          <div className="long-field">Od</div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field long-field-end"></div>
         </div>
-        <div className="cell cell-3 long-field"></div>
-        <div className="cell cell-4 long-field"></div>
-        <div className="cell cell-5 short-field table-row-2-cell-5"></div>
-        <div className="cell cell-6 medium-field table-row-2-cell-6"></div>
-        <div className="cell cell-7 medium-field table-row-2-cell-7"></div>
+        <div className="cell cell-3 table-row-2-cell-3">
+          <div className="long-field">Do</div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field long-field-end"></div>
+        </div>
+        <div className="cell cell-4 table-row-2-cell-4">
+          <div className="long-field">Vrsta prevoza</div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field"></div>
+          <div className="long-field long-field-end"></div>
+        </div>
+        <div className="cell cell-5 table-row-2-cell-5">
+          <div className="short-field short-field-row-2"></div>
+          <div className="short-field short-field-row-2"></div>
+          <div className="short-field short-field-row-2"></div>
+          <div className="short-field short-field-row-2"></div>
+          <div className="short-field short-field-row-2"></div>
+          <div className="short-field short-field-row-2"></div>
+          <div className="short-field short-field-row-2"></div>
+          <div className="short-field short-field-end short-field-row-2"></div>
+        </div>
+        <div className="cell cell-6 table-row-2-cell-6">
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2 medium-field-end"></div>
+        </div>
+        <div className="cell cell-7 table-row-2-cell-7">
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2"></div>
+          <div className="medium-field medium-field-row-2 medium-field-end"></div>
+        </div>
       </div>
       <div className="table-row table-row-3"></div>
       <div className="table-row table-row-4"></div>
