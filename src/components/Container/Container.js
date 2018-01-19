c5f1ea4a13873c75b138a9e06f151c6891f8c6b0
Celoslav <lucicmladen83@gmail.com>
2018-01-19 12:37:25 +0100

    Styled addUser, person and Container. Issue with scrollbar

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -85,7 +85,7 @@ class Container extends Component {
             return (
                 <div className="container">
                     <div className="users-heading">
-                        <h3>USERS</h3>
+                        <h3>Users</h3>
                         <IconButton
                             onClick={this.clickHandler}
                             className="add-person-icon-container"
