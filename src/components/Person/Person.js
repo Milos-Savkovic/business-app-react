e579a622549453fe672529c2ae77b08ea60d50e7
Stefan <stefansukara55@gmail.com>
2018-01-23 16:21:49 +0100

    Implemented service for get images for every user in container

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import './person.css';
 import RaisedButton from 'material-ui/RaisedButton';
+import {getImage} from '../../api/getImage';
 
 class Person extends Component {
+    
+    state={
+        image:'',
+    }
 
     handleDetailPerson(id) {
         this.props.clickHandlerDetail(id);
     }
+
+    getImage(){
+        getImage(this.props.id)
+           .then((url) => {
+            this.setState({
+                image: url,
+            });
+        }).catch(error => {
+            console.log(error);
+        })
+    }
     render() {
+        this.getImage();
         return (
             <div className="card">
-                <img src="https://cdn.dribbble.com/users/112117/screenshots/3792149/avatar-dribbble_1x.png" className="avatar" alt="Jane" />
+                <img src={this.state.image} className="avatar" alt="Jane" />
                 <div >
                     <h2 className="name">{`${this.props.firstName} ${this.props.lastName}`}</h2>
                     <p className="title">{this.props.position}</p>
