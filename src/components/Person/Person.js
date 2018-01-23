db2882eb1ffb11ebabc70e211daebdac18ecedac
Stefan <stefansukara55@gmail.com>
2018-01-23 16:49:45 +0100

    Added default picture for person without picture

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,33 +1,37 @@
 import React, { Component } from 'react';
 import './person.css';
 import RaisedButton from 'material-ui/RaisedButton';
-import {getImage} from '../../api/getImage';
+import { getImage } from '../../api/getImage';
 
 class Person extends Component {
-    
-    state={
-        image:'',
+
+    state = {
+        image: '',
     }
 
     handleDetailPerson(id) {
         this.props.clickHandlerDetail(id);
     }
 
-    getImage(){
+    getImage() {
         getImage(this.props.id)
-           .then((url) => {
-            this.setState({
-                image: url,
-            });
-        }).catch(error => {
-            console.log(error);
-        })
+            .then((url) => {
+                this.setState({
+                    image: url,
+                });
+            }).catch(error => {
+                console.log(error);
+            })
     }
     render() {
         this.getImage();
+        let picture;
+        if (this.state.image) picture = this.state.image;
+        else picture = "https://cdn.dribbble.com/users/112117/screenshots/3792149/avatar-dribbble_1x.png";
+
         return (
             <div className="card">
-                <img src={this.state.image} className="avatar" alt="Jane" />
+                <img src={picture} className="avatar" alt="Jane" />
                 <div >
                     <h2 className="name">{`${this.props.firstName} ${this.props.lastName}`}</h2>
                     <p className="title">{this.props.position}</p>
